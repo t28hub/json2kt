1cf53a908bc3f20faa8fe55faf94985da59df8bb
t28hub <t28oogle@gmail.com>
2022-02-05 22:16:45 +0900

    Add a simple action class for creating kotlin classes

---
+++ src/main/kotlin/io/t28/json2kotlin/idea/util/Project.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022 Tatsuya Maki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.t28.json2kotlin.idea.util
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+/**
+ * Whether the current project is available.
+ */
+@OptIn(ExperimentalContracts::class)
+fun Project?.isAvailable(): Boolean {
+    contract {
+        returns(true) implies (this@isAvailable != null)
+    }
+    return this != null && isInitialized && !isDisposed
+}
+
+/**
+ * Get [ProjectRootManager] from current project.
+ */
+fun Project.getProjectRootManager(): ProjectRootManager {
+    return ProjectRootManager.getInstance(this)
+}
