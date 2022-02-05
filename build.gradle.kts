d59496bd62733e1c551cf7bf98fc65ade3b97443
t28hub <t28oogle@gmail.com>
2022-02-05 14:56:21 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022 Tatsuya Maki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(deps.plugins.intellij)
+}
+
+group = "io.t28.json2kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    pluginName.set("Json2Kotlin")
+    version.set("2021.3.2")
+}
+
+tasks {
+    runIde {
+        autoReloadPlugins.set(true)
+    }
+
+    patchPluginXml {
+        changeNotes.set(
+            """
+            Add change notes here.<br>
+            <em>most HTML tags may be used</em>
+        """.trimIndent()
+        )
+    }
+}
